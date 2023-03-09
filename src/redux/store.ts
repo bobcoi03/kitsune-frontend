eebe78e70fb1b347bf9397c8ca64b8d2af992653
kryptopaul <pl4249t@gre.ac.uk>
2023-03-09 12:19:41 +0000

    cleaned up the types spaghetti

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,16 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-
-enum ActionType {
-  ADD_ORDER = "ADD_ORDER",
-  EDIT_ORDER_AMOUNT = "EDIT_ORDER_AMOUNT",
-  SET_TARGET_TOKEN = "SET_TARGET_TOKEN",
-  DELETE_ORDER = "DELETE_ORDER",
-  OPEN_MODAL = "OPEN_MODAL",
-  CLOSE_MODAL = "CLOSE_MODAL",
-  SET_CUSTOM_RECIPIENT = "SET_CUSTOM_RECIPIENT",
-  TOGGLE_CUSTOM_RECIPIENT = "TOGGLE_CUSTOM_RECIPIENT",
-  CLEAR_ALL_ORDERS = "CLEAR_ALL_ORDERS"
-}
+import { ActionType, AppState, Action } from "../types/types";
 
 // Define the reducer
 const initialState: AppState = {
